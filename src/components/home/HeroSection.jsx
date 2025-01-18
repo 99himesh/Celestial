bf2f2f3b07446660533845cd75a1ca8f128fa333
himesh kashyap <himeshkashyap347@gmail.com>
2025-01-18 15:58:46 +0530

    add

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import video from "../../assets/video.mp4"
 // This is my hero section .start here
 const HeroSection=()=>{
+  const video="https://s31.aconvert.com/convert/p3r68-cdx67/fm91q-c01pr.mp4"
     return(
       <div className="pt-[134px]">
         <video className="w-full  md:h-[500px]" muted loop autoPlay>
