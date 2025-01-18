c4d2384c167f46f119942429a5cf1ffac4a63480
himesh kashyap <himeshkashyap347@gmail.com>
2025-01-18 18:50:31 +0530

    update

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from "react";
 import video from "../../assets/video.mp4"
+import Loading from "../loading/Loading";
 // This is my hero section .start here
 const HeroSection=()=>{
+  const [loading,setloading]=useState(false)
+  const handleVideoLoaded = () => {
+    setloading(false);
+  };
 
-
-  
+  if(loading) return <Loading/>
     return(
       <div className="pt-[134px]">
-        <video className="w-full  md:h-[500px]"  muted loop autoPlay>
+        <video onLoadedData={handleVideoLoaded} className="w-full  md:h-[500px]"  muted loop autoPlay>
         <source   src={video} type="video/mp4"/>
         <source  src={video} type="video/ogg"/>
         Your browser does not support the video tag.
