6ac9e65ca3619c4edee156aa775ff5d7c14d8875
himesh kashyap <himeshkashyap347@gmail.com>
2025-01-18 18:42:39 +0530

    update

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import video from "../../assets/video.mp4"
 // This is my hero section .start here
 const HeroSection=()=>{
-  const video="https://s31.aconvert.com/convert/p3r68-cdx67/fm91q-c01pr.mp4"
+
+
+  
     return(
       <div className="pt-[134px]">
-        <video className="w-full  md:h-[500px]" muted loop autoPlay>
+        <video className="w-full  md:h-[500px]"  muted loop autoPlay>
         <source   src={video} type="video/mp4"/>
         <source  src={video} type="video/ogg"/>
         Your browser does not support the video tag.
