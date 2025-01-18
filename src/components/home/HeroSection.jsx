e013ede500180c4b7f8ca924065987282119f683
himesh kashyap <himeshkashyap347@gmail.com>
2025-01-18 15:44:18 +0530

    add

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import video from "../../assets/video.mp4"
 // This is my hero section .start here
 const HeroSection=()=>{
     return(
       <div className="pt-[134px]">
-        <video className="w-full md:h-[500px] " controls>
-        <source src="https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4" type="video/mp4"/>
-        <source src="https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4" type="video/ogg"/>
+        <video className="w-full md:h-[500px]" muted loop autoPlay>
+        <source   src={video} type="video/mp4"/>
+        <source  src={video} type="video/ogg"/>
         Your browser does not support the video tag.
       </video>
       
