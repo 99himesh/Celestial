fd9e72495712100f3f20705c7a19aec0a589c53c
himesh kashyap <himeshkashyap347@gmail.com>
2025-02-03 16:56:27 +0530

    update card

--- src/components/home/CategarySection.jsx
+++ src/components/home/CategarySection.jsx
@@ -44,10 +44,10 @@ const CategarySection = () => {
         </div>
 
         <SubCategary/>
-        <div>
+        <div className="w-[80%] mx-auto">
         <h3 className=' text-[#214344] md:font-[500] md:text-[65px] text-[20px] text-center'>Top Sellers</h3>
         <h3 className=' text-[#214344] font-[600] text-[20px] text-center'>Our Most Loved Products</h3>
-        <div className='md:flex  md:gap-5 justify-start md:justify-between md:px-20 px-5 items-center pt-5'>
+        <div className='md:flex  md:gap-5 justify-start md:justify-between  px-5 items-center pt-5'>
           <div className='md:flex  md:gap-5 items-center md:justify-start '>
             <h4 className='text-[24px] font-bold '>All Products</h4>
           <p className='text-[16px] font-semibold text-gray-500'>Dont miss out on this weeks deals</p>
