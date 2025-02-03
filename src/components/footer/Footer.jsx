a3a0d1fac453eae959c9074bf334fe4ebda6aa49
himesh kashyap <himeshkashyap347@gmail.com>
2025-02-03 17:25:01 +0530

    update card

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -15,6 +15,7 @@ import {
   FaYoutube,
   FaYoutubeSquare,
 } from "react-icons/fa";
+import { TbBrandLinkedin } from "react-icons/tb";
 const Footer=()=>{
     const year=new Date().getFullYear();
     const dispatch=useDispatch();
@@ -47,34 +48,34 @@ const Footer=()=>{
             </Col>
             <Col xl={6} lg={6} md={8} sm={12} xs={24}>
             <div className=""  >
-                <h6 className="text-[24px]  text-[#F0D5A0]" >Let Us Help You</h6>
+                <h6 className="text-[24px]  text-[#F0D5A0]" >LET US HELP YOU</h6>
                 <div className="flex flex-col gap-3 pt-10">
-                    <Link to={"/"} className="text-[#fff] hover:text-[#fff] text-[20px] ">Home</Link>
-                    <Link to={"/shop"} className="text-[#fff] hover:text-[#fff] text-[20px] ">Shop</Link>
-                    <Link className="text-[#fff] hover:text-[#fff] text-[20px] ">Cart</Link>
-                    <Link className="text-[#fff] hover:text-[#fff] text-[20px] ">Orders</Link>
-                    <Link className="text-[#fff] hover:text-[#fff] text-[20px] ">About Us</Link>
-                    <Link className="text-[#fff] hover:text-[#fff] text-[20px] ">Wishlist</Link>
-                    <Link className="text-[#fff] hover:text-[#fff] text-[20px] ">Order Traking</Link>
-                    <Link className="text-[#fff] hover:text-[#fff] text-[20px] ">Blog</Link>
-                    <Link className="text-[#fff] hover:text-[#fff] text-[20px] ">FAQ</Link>
-                    <Link className="text-[#fff] hover:text-[#fff] text-[20px] ">Contact Us</Link>
+                    <Link to={"/"} className="text-[#fff] hover:text-[#fff] text-[16px] ">Home</Link>
+                    <Link to={"/shop"} className="text-[#fff] hover:text-[#fff] text-[16px] ">Shop</Link>
+                    <Link className="text-[#fff] hover:text-[#fff] text-[16px] ">Cart</Link>
+                    <Link className="text-[#fff] hover:text-[#fff] text-[16px] ">Orders</Link>
+                    <Link className="text-[#fff] hover:text-[#fff] text-[16px] ">About Us</Link>
+                    <Link className="text-[#fff] hover:text-[#fff] text-[16px] ">Wishlist</Link>
+                    <Link className="text-[#fff] hover:text-[#fff] text-[16px] ">Order Traking</Link>
+                    <Link className="text-[#fff] hover:text-[#fff] text-[16px] ">Blog</Link>
+                    <Link className="text-[#fff] hover:text-[#fff] text-[16px] ">FAQ</Link>
+                    <Link className="text-[#fff] hover:text-[#fff] text-[16px] ">Contact Us</Link>
                     
                 </div>
             </div>
             </Col>
             <Col xl={6} lg={6} md={8} sm={12} xs={24}>
             <div className=""   >
-                <h6 className="text-[24px]  text-[#F0D5A0]">The Collection</h6>
+                <h6 className="text-[24px]  text-[#F0D5A0]">THE COLLECTION</h6>
                 <div className="flex flex-col gap-3 pt-10 ">
-                    <Link className="text-[#fff] hover:text-[#fff] text-[20px] cursor-pointer">Signature Collection</Link>
-                    <Link className="text-[#fff] hover:text-[#fff] text-[20px] cursor-pointer">Personalized Pieces</Link>
-                    <Link className="text-[#fff] hover:text-[#fff] text-[20px] cursor-pointer">Regular Upkeep</Link>
-                    <Link className="text-[#fff] hover:text-[#fff] text-[20px] cursor-pointer" onClick={()=>{filterSubcategary("nackeless")}}>Nackles</Link>
-                    <Link className="text-[#fff] hover:text-[#fff] text-[20px] cursor-pointer" onClick={()=>{filterSubcategary("earings")}}>Earring</Link>
-                    <Link className="text-[#fff] hover:text-[#fff] text-[20px] cursor-pointer" onClick={()=>{filterSubcategary("pendents")}}>Pendent</Link>
-                    <Link className="text-[#fff] hover:text-[#fff] text-[20px] cursor-pointer" onClick={()=>{filterSubcategary("bracelets")}}>Bracelet</Link>
-                    <Link className="text-[#fff] hover:text-[#fff] text-[20px] cursor-pointer" onClick={()=>{filterSubcategary("ring")}}>Rings</Link>
+                    <Link className="text-[#fff] hover:text-[#fff] text-[16px] cursor-pointer">Signature Collection</Link>
+                    <Link className="text-[#fff] hover:text-[#fff] text-[16px] cursor-pointer">Personalized Pieces</Link>
+                    <Link className="text-[#fff] hover:text-[#fff] text-[16px] cursor-pointer">Regular Upkeep</Link>
+                    <Link className="text-[#fff] hover:text-[#fff] text-[16px] cursor-pointer" onClick={()=>{filterSubcategary("nackeless")}}>Nackles</Link>
+                    <Link className="text-[#fff] hover:text-[#fff] text-[16px] cursor-pointer" onClick={()=>{filterSubcategary("earings")}}>Earring</Link>
+                    <Link className="text-[#fff] hover:text-[#fff] text-[16px] cursor-pointer" onClick={()=>{filterSubcategary("pendents")}}>Pendent</Link>
+                    <Link className="text-[#fff] hover:text-[#fff] text-[16px] cursor-pointer" onClick={()=>{filterSubcategary("bracelets")}}>Bracelet</Link>
+                    <Link className="text-[#fff] hover:text-[#fff] text-[16px] cursor-pointer" onClick={()=>{filterSubcategary("ring")}}>Rings</Link>
                 </div>
 
 
@@ -94,28 +95,28 @@ const Footer=()=>{
             </Col>
             <Col xl={6} lg={6} md={8} sm={12} xs={24}>
             <div className=""  >
-                <h6 className="text-[24px]  text-[#F0D5A0]">Contact Details</h6>
+                <h6 className="text-[24px]  text-[#F0D5A0]">CONTACT DETAILS</h6>
                 <div className="flex flex-col gap-3 pt-10">
-                    <Link className="text-[#fff] hover:text-[#fff] text-[20px] ">hello@zoci.in</Link>
-                    <Link className="text-[#fff] hover:text-[#fff] text-[20px] ">Monday – Friday: 9:00AM - 20:00PM</Link>
-                    <Link className="text-[#fff] hover:text-[#fff] text-[20px] ">Santacruz East, Mumbai, Maharashtra 400055</Link>
+                    <Link className="text-[#fff] hover:text-[#fff] text-[16px] ">hello@zoci.in</Link>
+                    <Link className="text-[#fff] hover:text-[#fff] text-[16px] ">Monday – Friday: 9:00AM - 20:00PM</Link>
+                    <Link className="text-[#fff] hover:text-[#fff] text-[16px] ">Santacruz East, Mumbai, Maharashtra 400055</Link>
                 </div>
             </div>
             <div className="pt-10"  >
                 <h6 className="text-[24px]  text-[#F0D5A0]">HELP</h6>
                 <div className="flex flex-col gap-3 pt-10">
-                    <Link className="text-[#fff] hover:text-[#fff] text-[20px] ">Jewelry Care Guide</Link>
-                    <Link className="text-[#fff] hover:text-[#fff] text-[20px] ">Privacy Policys</Link>
-                    <Link className="text-[#fff] hover:text-[#fff] text-[20px] ">Shipping Policy</Link>
-                    <Link className="text-[#fff] hover:text-[#fff] text-[20px] ">Terms and Conditions</Link>
-                    <Link className="text-[#fff] hover:text-[#fff] text-[20px] ">Refund and exchange policy</Link>
+                    <Link className="text-[#fff] hover:text-[#fff] text-[16px] ">Jewelry Care Guide</Link>
+                    <Link className="text-[#fff] hover:text-[#fff] text-[16px] ">Privacy Policys</Link>
+                    <Link className="text-[#fff] hover:text-[#fff] text-[16px] ">Shipping Policy</Link>
+                    <Link className="text-[#fff] hover:text-[#fff] text-[16px] ">Terms and Conditions</Link>
+                    <Link className="text-[#fff] hover:text-[#fff] text-[16px] ">Refund and exchange policy</Link>
                 </div>
             </div>
             </Col>
         </Row>
         </div>
         <Row className="pt-5" >
-             <div className="flex w-full gap-5 justify-center  py-7 border-b-[0.5px] border-t-[1px] border-gray-500"  >
+             <div className="flex w-full gap-5 justify-center items-center py-7 border-b-[0.5px] border-t-[1px] border-gray-500"  >
                 <h4 className="text-[18px] font-bold text-[#F0D5A0] tracking-wide  ">Follow US On</h4>
                 {/* <FacebookOutlined style={{fontSize:"24px",color:"#F0D5A0"}} />
                 <InstagramOutlined style={{fontSize:"24px",color:"#F0D5A0"}} />
@@ -128,7 +129,8 @@ const Footer=()=>{
                               <FaPinterestP style={{ fontSize: "20px", color: "#F0D5A0" }} />
                               <FaYoutubeSquare style={{ fontSize: "20px", color: "#F0D5A0" }} />
                               <WhatsAppOutlined  style={{ fontSize: "20px", color: "#F0D5A0" }} />
-                              <LinkedinOutlined style={{ fontSize: "20px", color: "#F0D5A0" }}/>
+                              {/* <LinkedinOutlined style={{ fontSize: "20px", color: "#F0D5A0" }}/> */}
+                              <TbBrandLinkedin style={{ fontSize: "20px", color: "#F0D5A0" }} />
              </div>
              <div className="flex flex-wrap md:justify-between justify-center max-md:gap-5 md:px-20 px-5  w-full py-7"  >
                 <div>
